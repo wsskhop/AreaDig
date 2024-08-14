295a08673702cf8d84b8f6e7561463200e00fd1b
wsskhop <emptyset_zh-cn@outlook.com>
2024-08-14 15:35:00 +0800

    Initial commit

---
+++ src/main/java/cn/marvelpixel/AreaDig.java
@@ -0,0 +1,17 @@
+package cn.marvelpixel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AreaDig extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
